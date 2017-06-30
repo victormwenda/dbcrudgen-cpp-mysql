f77e6fe555b14a7bfea3c9528cef9be6fc1bcf5e
Victor Mwenda <vmwenda.vm@gmail.com>
2017-06-30 23:14:36 +0300

    Database Connection : Added different database types connection requirements

--- core/database/connection/MSSQLDatabaseConnection.h
+++ core/database/connection/MSSQLDatabaseConnection.h
@@ -11,12 +11,33 @@
 #ifndef DBCRUDGEN_CPP_MSSQLDATABASECONNECTION_H
 #define DBCRUDGEN_CPP_MSSQLDATABASECONNECTION_H
 
+#include <iostream>
 
 /**
 * MSSQLDatabaseConnection
 * */
 class MSSQLDatabaseConnection {
 
+private:
+    std::string host;
+    std::string username;
+    std::string password;
+public:
+    MSSQLDatabaseConnection(const std::string &host, const std::string &username,
+                            const std::string &password) :
+            host(host), username(username), password(password) {}
+
+    const std::string &getHost() const {
+        return host;
+    }
+
+    const std::string &getUsername() const {
+        return username;
+    }
+
+    const std::string &getPassword() const {
+        return password;
+    }
 };
 
 
--- core/database/connection/OracleDatabaseConnection.h
+++ core/database/connection/OracleDatabaseConnection.h
@@ -11,12 +11,60 @@
 #ifndef DBCRUDGEN_CPP_ORACLEDATABASECONNECTION_H
 #define DBCRUDGEN_CPP_ORACLEDATABASECONNECTION_H
 
+#include <iostream>
 
 /**
 * OracleDatabaseConnection
 * */
 class OracleDatabaseConnection {
 
+private:
+
+    std::string connectString;
+
+    std::string host;
+    std::string port;
+    std::string username;
+    std::string password;
+    std::string serviceName;
+
+    std::string getConnectStringTemplate() const {
+        return "${USERNAME}/${PASSWORD}@[//]${HOST}[:${PORT}][/${SERVICE_NAME}]";
+    }
+
+public:
+    OracleDatabaseConnection(const std::string &host, const std::string &port, const std::string &username,
+                             const std::string &password, const std::string &serviceName)
+            : host(host), port(port),
+              username(username),
+              password(password),
+              serviceName(serviceName) {}
+
+    OracleDatabaseConnection(const std::string &connectString) : connectString(connectString) {}
+
+    const std::string &getConnectString() const {
+        return connectString;
+    }
+
+    const std::string &getHost() const {
+        return host;
+    }
+
+    const std::string &getPort() const {
+        return port;
+    }
+
+    const std::string &getUsername() const {
+        return username;
+    }
+
+    const std::string &getPassword() const {
+        return password;
+    }
+
+    const std::string &getServiceName() const {
+        return serviceName;
+    }
 };
 
 
