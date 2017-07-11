57d2ae71a6f0d30197ae7b7135fc4a76c27ccf7e
Victor Mwenda <vmwenda.vm@gmail.com>
2017-07-11 14:26:56 +0300

    Created API's for performing MYSQL Queries

--- core/database/connectors/MYSQLDatabaseConnector.h
+++ core/database/connectors/MYSQLDatabaseConnector.h
@@ -12,6 +12,9 @@
 #define DBCRUDGEN_CPP_MYSQLDATABASECONNECTOR_H
 
 
+#include <cppconn/connection.h>
+#include <cppconn/driver.h>
+
 #include "DatabaseConnector.h"
 #include "credentials/MYSQLDatabaseConnectionParams.h"
 
@@ -23,26 +26,66 @@ class MYSQLDatabaseConnector : public DatabaseConnector {
 private:
     bool autoConnect;
     MYSQLDatabaseConnectionParams connectionParams;
+    sql::Driver *driver;
+    sql::Connection *connection;
 public:
     explicit MYSQLDatabaseConnector(MYSQLDatabaseConnectionParams connectionParams,
                                     bool autoConnect)
-            : connectionParams{connectionParams}, autoConnect{autoConnect} {}
+            : connectionParams{connectionParams}, autoConnect{autoConnect} {
+
+        driver = get_driver_instance();
+
+        if (autoConnect) { open(); }
+    }
+
+    /**
+       * Get the database driver
+       * @return
+       */
+    sql::Driver &getDriver() {
+        if (driver == nullptr) {
+            driver = get_driver_instance();
+        }
+        return *driver;
+    }
+
+    /**
+        * Get the connection
+        * @return
+        */
+    sql::Connection &getConnection() {
+        return *connection;
+    }
 
     bool open() override {
-        return false;
+
+        if (connection == nullptr) {
+            sql::SQLString host = connectionParams.getHost();
+            sql::SQLString user = connectionParams.getUsername();
+            sql::SQLString password = connectionParams.getPassword();
+            connection = driver->connect(host, user, password);
+        }
+
+        return connection != nullptr;
     }
 
     bool isOpen() override {
-        return false;
+        return open();
     }
 
     bool close() override {
-        return false;
+        delete connection;
+        return connection == nullptr && driver == nullptr;
     }
 
     void onError(std::string errorLevel, std::string errorMessage, bool logError) override {
         DatabaseConnector::onError(errorLevel, errorMessage, logError);
     }
+
+    ~MYSQLDatabaseConnector() {
+        if (isOpen()) { close(); }
+    }
+
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include "core/database/connectors/MYSQLDatabaseConnector.h"
+#include "core/database/executors/MYSQLQueryExecutor.h"
+
+#include <cppconn/statement.h>
+#include <cppconn/resultset.h>
 
 using namespace std;
 
-int main(void)
-{
+int main(void) {
+
+    MYSQLDatabaseConnectionParams connectionParams{"tcp://127.0.0.1:3306",
+                                                   "root", "root3358"};
+
+    MYSQLDatabaseConnector connector{connectionParams, true};
+
+    if (connector.isOpen()) {
+        cout << "Database is open " << endl;
+    }
+
+    MYSQLQueryExecutor executor{&connector, "dbcrudgen"};
+
+    sql::ResultSet *resultSet = &executor.exec("SELECT * FROM bug_logger;");
+
+    while (resultSet->next()) {
+        std::string stackTrace = resultSet->getString("stack_trace");
+        cout << "Stack trace : " << stackTrace << endl;
+    }
+    delete (resultSet);
     return EXIT_SUCCESS;
 }
