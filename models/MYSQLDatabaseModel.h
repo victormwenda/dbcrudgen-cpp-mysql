fc9a850fb2fb138021cbee4ed962bbc5175c98ff
Victor Mwenda <vmwenda.vm@gmail.com>
2017-08-05 21:44:21 +0300

    Created database model creators

---
+++ models/MYSQLDatabaseModel.h
@@ -0,0 +1,23 @@
+//
+// @author Victor Mwenda
+// @author vmwenda.vm@gmail.com
+//
+// Created by victor on 8/5/17.
+// C/C++ Laptop
+//
+
+
+
+#ifndef DBCRUDGEN_CPP_MYSQLDATABASEMODEL_H
+#define DBCRUDGEN_CPP_MYSQLDATABASEMODEL_H
+
+
+//
+// MYSQLDatabaseModel
+// //
+class MYSQLDatabaseModel {
+
+};
+
+
+#endif //DBCRUDGEN_CPP_MYSQLDATABASEMODEL_H
