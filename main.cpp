e0b3c2e0dca9cd860ff3827ba6d47de45a841243
Victor Mwenda <vmwenda.vm@gmail.com>
2017-08-16 16:10:06 +0300

    Linked project with OCI Libs

--- main.cpp
+++ main.cpp
@@ -6,24 +6,15 @@ using namespace oracle::occi;
 int main(int argc, char **argv) {
     const std::string userName = "victor";
     const std::string password = "root3358";
-    const std::string connectionString = "//localhost:1521/xe";
+    const std::string connectionString = "jdbc:oracle:thin:@//victor:1522/XE";
+
 
     Environment *env = Environment::createEnvironment();
+    Connection *conn = env->createConnection(userName, password, connectionString);
 
-    Connection *conn = env->createConnection(
-            userName, password, connectionString);
-    Statement *stmt = conn->createStatement(
-            "SELECT * FROM BUG_LOGGER;");
-    ResultSet *rs = stmt->executeQuery();
-    rs->next();
-    int bugId = rs->getInt(1);
-    std::cout << "Bug id : " << bugId << std::endl;
-
-    stmt->closeResultSet(rs);
-    conn->terminateStatement(stmt);
     env->terminateConnection(conn);
-
     Environment::terminateEnvironment(env);
+
 
     return EXIT_SUCCESS;
 }
