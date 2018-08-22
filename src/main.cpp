d77d4f6a3f2d1e60a2ca1f0c7d7252251970e698
Victor Mwenda <vmwenda.vm@gmail.com>
2018-08-22 20:58:04 +0300

    Added new oracle libs for linux

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,26 @@ int main(int argc, char **argv) {
 
     const std::string userName = "mwenda";
     const std::string password = "root3358";
-    const std::string connectionString = "jdbc:oracle:thin:@//mwenda:1522/XE";
+    const std::string connectionString = "//mwenda:1521/xe";
 
 
-    oracle::occi::Environment *env = oracle::occi::Environment::createEnvironment();
-    oracle::occi::Connection *conn = env->createConnection(userName, password, connectionString);
+    try {
+        oracle::occi::Environment *env = oracle::occi::Environment::createEnvironment();
+        oracle::occi::Connection *conn = env->createConnection(userName, password, connectionString);
 
-    env->terminateConnection(conn);
-    oracle::occi::Environment::terminateEnvironment(env);
+        oracle::occi::Statement *statement = conn->createStatement();
+        oracle::occi::ResultSet *resultSet = statement->executeQuery("select * from bug_logger");
+
+        while (resultSet->next()) {
+            std::cout << resultSet->getString(4) << std::endl;
+        }
+
+        env->terminateConnection(conn);
+        oracle::occi::Environment::terminateEnvironment(env);
+    } catch (oracle::occi::SQLException& exception) {
+        std::cout << "ERROR : " << exception.getErrorCode() << " : " << exception.getMessage();
+    }
+
 
     return EXIT_SUCCESS;
 }
