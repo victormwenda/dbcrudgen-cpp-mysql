4e5705e627d5c44f642ae3769f604af8deeb4a2c
Victor Mwenda <vmwenda.vm@gmail.com>
2018-11-22 23:01:32 +0300

    Merge origin/db-model-creators-oracle

---
+++ src/utils/OracleTags.h
@@ -0,0 +1,27 @@
+//
+// @author Victor Mwenda
+// @author vmwenda.vm@gmail.com
+//
+// Created by victor on 11/19/18.
+// C/C++ Laptop
+//
+
+
+
+#ifndef DBCRUDGEN_CPP_ORACLETAGS_H
+#define DBCRUDGEN_CPP_ORACLETAGS_H
+
+
+//
+// OracleTags
+// //
+class OracleTags {
+public :
+    constexpr static char *OWNER = "${OWNER}";
+    constexpr static char *OBJECT = "${OBJECT}";
+    constexpr static char *SCHEMA = "${SCHEMA}";
+    constexpr static char *OBJECT_NAME = "${OBJECT_NAME}";
+};
+
+
+#endif //DBCRUDGEN_CPP_ORACLETAGS_H
