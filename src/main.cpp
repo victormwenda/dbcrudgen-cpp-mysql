94a877730632523135dec33c0eb51cd8a74d743b
Victor Mwenda <vmwenda.vm@gmail.com>
2019-05-25 12:37:58 +0300

    Testing configuring Oracle development on home workstation

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,37 @@
 int main(int argc, char **argv) {
 
 
-    const std::string userName = "victor";
+    const std::string userName = "mwenda";
     const std::string password = "root3358";
-    const std::string connectionString = "//victor:1522/XE";
+    const std::string connectionString = "//victor:1521/XE";
 
 
     oracle::occi::Environment *env = oracle::occi::Environment::createEnvironment();
     oracle::occi::Connection *conn = env->createConnection(userName, password, connectionString);
 
-    OracleDatabaseModel databaseModel{userName, password, connectionString};
-    auto ddl = databaseModel.getTableDDL("VICTOR", "BUG_LOGGER");
+    oracle::occi::Statement *statement = conn->createStatement("SELECT * FROM BUG_LOGGER");
 
-    std::cout << ddl << std::endl;
+    oracle::occi::ResultSet *resultSet = statement->executeQuery("SELECT * FROM BUG_LOGGER");
 
-    for (int i = 0; i < 0; i++) {
-        std::cout << i << std::endl;
+    while (resultSet->next()) {
+        std::string stack_trace = resultSet->getString(4);
+        std::cout << stack_trace << std::endl;
     }
 
+    /* OracleDatabaseModel databaseModel{userName, password, connectionString};
+     auto ddl = databaseModel.getAllDBAUsers();
+
+     for (const auto &username : ddl) {
+         std::cout << username.username << std::endl;
+     }
+
+
+     for (int i = 0; i < 0; i++) {
+         std::cout << i << std::endl;
+     }*/
+
+    env->terminateConnection(conn);
+    oracle::occi::Environment::terminateEnvironment(env);
 
     return EXIT_SUCCESS;
 }
