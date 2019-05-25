8d5a3d2fc9207676304b98ecddd6efb97a56734b
Victor Mwenda <vmwenda.vm@gmail.com>
2019-05-25 13:14:19 +0300

    Oracle::Get all dba users(GRANT SELECT ON dba_users TO )

--- src/main.cpp
+++ src/main.cpp
@@ -13,26 +13,14 @@ int main(int argc, char **argv) {
     oracle::occi::Environment *env = oracle::occi::Environment::createEnvironment();
     oracle::occi::Connection *conn = env->createConnection(userName, password, connectionString);
 
-    oracle::occi::Statement *statement = conn->createStatement("SELECT * FROM BUG_LOGGER");
 
-    oracle::occi::ResultSet *resultSet = statement->executeQuery("SELECT * FROM BUG_LOGGER");
-
-    while (resultSet->next()) {
-        std::string stack_trace = resultSet->getString(4);
-        std::cout << stack_trace << std::endl;
-    }
-
-    /* OracleDatabaseModel databaseModel{userName, password, connectionString};
+    OracleDatabaseModel databaseModel{userName, password, connectionString};
      auto ddl = databaseModel.getAllDBAUsers();
 
      for (const auto &username : ddl) {
          std::cout << username.username << std::endl;
      }
 
-
-     for (int i = 0; i < 0; i++) {
-         std::cout << i << std::endl;
-     }*/
 
     env->terminateConnection(conn);
     oracle::occi::Environment::terminateEnvironment(env);
