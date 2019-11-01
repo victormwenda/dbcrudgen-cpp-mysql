c29c74979caf9fa1f334f232e55923f4333796fa
Victor Rwanda <vrwanda@safaricom.co.ke>
2019-11-01 16:11:17 +0300

    Reading the columns in a table

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,28 @@
 int main(int argc, char **argv) {
 
 
-    const std::string userName = "mwenda";
+    const std::string userName = "victor";
     const std::string password = "root3358";
-    const std::string connectionString = "//victor:1521/XE";
+
+    const std::string connectionString = "//localhost:1521/XE";
 
 
     oracle::occi::Environment *env = oracle::occi::Environment::createEnvironment();
     oracle::occi::Connection *conn = env->createConnection(userName, password, connectionString);
 
 
-    OracleDatabaseModel databaseModel{userName, password, connectionString};
-     auto ddl = databaseModel.getAllDBAUsers();
+    oracle::occi::MetaData metaData = conn->getMetaData("BUG_LOGGER", oracle::occi::MetaData::PTYPE_TABLE);
 
-     for (const auto &username : ddl) {
-         std::cout << username.username << std::endl;
-     }
+    std::vector<oracle::occi::MetaData> columns = metaData.getVector(oracle::occi::MetaData::ATTR_LIST_COLUMNS);
+
+    for(auto& column : columns) {
+        std::cout << column.getString(oracle::occi::MetaData::ATTR_NAME) << " : ";
+        std::cout << column.getInt(oracle::occi::MetaData::ATTR_DATA_TYPE) << "\n";
+    }
 
 
-    env->terminateConnection(conn);
-    oracle::occi::Environment::terminateEnvironment(env);
+    //env->terminateConnection(conn);
+    //oracle::occi::Environment::terminateEnvironment(env);
 
     return EXIT_SUCCESS;
 }
